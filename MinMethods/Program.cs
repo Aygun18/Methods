9005aab78cfde512c934dc292204bd0e43f3c13c
Aygun <sahmuradova18@gmail.com>
2023-03-29 00:33:45 +0400

    Methods

---
+++ MinMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+namespace MinMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 5, 1, 8, 2 };
+            Console.WriteLine(Min(array));
+        }
+
+        public static int Min(int[] array)
+        {
+            int min = array[0];
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+        }
+    }
+
+}
+
---
+++ Overload_Tack/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Overload_Tack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Area(2, 3, 4);
+            Area1(4, 5);
+        }
+        public static void Area(int r, int p = 3)
+        {
+            int S = 0;
+            S = p * r * r;
+            Console.WriteLine( S);
+           
+         
+        }
+        public static void Area1(int a, int b)
+        {
+            int S = 0;
+            S = a * b;  
+            Console.WriteLine( S);
+           
+        }
+        public static void Area(int a , int b, int c)
+        {
+            int S = 0;
+            S= 2*(a*b+a*c+b*c);
+            Console.WriteLine( S);
+            
+        }
+        public static void Area(int a, int b, int c, int r) 
+        {
+            int p = 0;
+            int S = 0;
+            p = (a + b + c) / 2;
+            S = p * r;
+            Console.WriteLine(p);
+            Console.WriteLine(S);
+           
+        }
+       
+    }
+}
